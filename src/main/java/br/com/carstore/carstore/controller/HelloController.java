c528a7e27435711705f33c28c5b11f3bfbf9316e
Paulo <pcrrp.pereira@gmail.com>
2026-03-12 21:58:08 -0300

    Adicionando o arquivo da atividade

---
+++ src/main/java/br/com/carstore/carstore/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.carstore.carstore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "<h1>Hello World</h1>";
+    }
+}
