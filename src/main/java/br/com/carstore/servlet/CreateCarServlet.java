0d60adef0af20e67f0652a0b446da317a1146094
Paulo <pcrrp.pereira@gmail.com>
2026-04-30 16:36:22 -0300

    Adicionando continuidade do laboratorio 6, mas ainda o banco de dados h2 não conecta

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,28 @@
+package br.com.carstore.servlet;
+
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String carName = req.getParameter("car-name");
+
+        Car car = new Car(carName);
+
+        new CarDao().createCar(car);
+
+        req.getRequestDispatcher("index.html").forward(req, resp);
+
+    }
+
+}
