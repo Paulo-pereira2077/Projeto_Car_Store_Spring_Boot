4bceb1cb9db3db68d664c0b42b8baf8d4725a14e
Paulo <pcrrp.pereira@gmail.com>
2026-05-14 00:11:48 -0300

    Adicionando segurança para os metodos do admin, impedindo que o user consiga acessar

--- src/main/java/br/com/carstore/controller/AdminController.java
+++ src/main/java/br/com/carstore/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,7 +33,7 @@ public class AdminController {
 
     @PreAuthorize("hasRole('ADMIN')") // <-- Bloqueia a criação/edição
     @PostMapping("/admin/cars")
-    public String createCar(CarDTO carDTO, BindingResult result) {
+    public String createCar(@ModelAttribute CarDTO carDTO, BindingResult result) {
         if (carDTO.getId() != null && !carDTO.getId().isEmpty()) {
             service.update(carDTO.getId(), carDTO);
         } else {
